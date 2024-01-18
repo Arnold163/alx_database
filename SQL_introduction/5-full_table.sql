37b1c4ea99dcbeb4320a260f7d3c8b2b2e592075
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-19 01:37:53 +0200

    update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS first_table(
     c CHAR(1) DEFAULT NULL,
     created_at DATE DEFAULT NULL,
     PRIMARY KEY (id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    );
 
 
