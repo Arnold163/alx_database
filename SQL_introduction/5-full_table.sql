53b277dcf7fff5663f801a741db88ac2383becd0
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-19 01:42:47 +0200

    update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,7 @@
 -- full description
 SHOW DATABASES;
 USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) DEFAULT NULL,
-    c CHAR(1) DEFAULT NULL,
-    created_at DATE DEFAULT NULL,
-    PRIMARY KEY (id)
-    );
+SHOW CREATE TABLE first_table;
+
 
 
