695bce16383d1cbc0d843dca71ecda7db0f96883
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-19 00:53:41 +0200

    update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -5,5 +5,15 @@ CREATE TABLE IF NOT EXISTS first_table(
     id INT PRIMARY KEY,
     name VARCHAR(256)
 );
-
+SELECT
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_NAME = 'first_table';
 
