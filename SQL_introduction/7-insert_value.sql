55d2e5d6e1bcd6fcfa406ce18b90aff4060341fd
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-19 09:46:30 +0200

    update

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -2,5 +2,6 @@
 SHOW DATABASES;
 USE hbtn_0c_0;
 SHOW TABLES;
-INSERT INTO first_table (id, name) VALUES (89, 'Holberton School')
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
+SELECT * FROM first_table;
 
