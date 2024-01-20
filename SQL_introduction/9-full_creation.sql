0866299a4bb8a1e6f5ad4ca31e001e70377159fd
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-20 14:20:40 +0200

    update

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,19 @@
--- full creation
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table(
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
+
+-- Switch to the specified database
+USE hbtn_test_db_9;
+
+-- Create second_table if it does not exist
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES 
-    (1, 'john', 10),
+
+-- Insert records into second_table
+INSERT INTO second_table (id, name, score) VALUES 
+    (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
     (4, 'George', 8);
