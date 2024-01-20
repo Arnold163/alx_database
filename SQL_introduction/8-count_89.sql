4ec28e4eb5cc18daf4d13767675046776559e612
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-20 20:54:09 +0200

    update

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,10 +1,11 @@
 -- mysql count
 SHOW DATABASES;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
-SHOW TABLES;
-SELECT COUNT(*) AS record_count 
+SELECT COUNT(*) AS record_count
 FROM first_table
 WHERE id = 89;
+
 
 
 
