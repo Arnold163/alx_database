45a4361789044214e080703fa79d476fdf97678a
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-21 23:18:13 +0200

    update

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS states (
 );
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
+    state_id INT,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
     );
