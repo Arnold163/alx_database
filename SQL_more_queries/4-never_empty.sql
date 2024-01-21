07c9646328ac705507fc8317920c63d588cb7db7
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-21 13:02:55 +0200

    update

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,8 @@
 -- ID cannot be NULL
 SHOW DATABASES;
-USE hbtn_0d_2;
+USE hbtn_test_db_4;
 CREATE TABLE IF NOT EXISTS id_not_null(
     id INT DEFAULT 1,
     name VARCHAR(256),
     PRIMARY KEY (id)
-
 );
