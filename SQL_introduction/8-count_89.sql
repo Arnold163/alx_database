db1ba6103112f96633780068801f290d57c9d351
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-21 11:16:10 +0200

    My privileges

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -2,9 +2,8 @@
 SHOW DATABASES;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
-SELECT COUNT(*) AS record_count
-FROM first_table
-WHERE id = 89;
+SHOW TABLES;
+
 
 
 
---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- privileges now, lets get sqlyy
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
