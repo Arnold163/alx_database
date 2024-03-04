6dae75c705d67be61dfb53e5674b0d1175076a52
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-04 12:53:32 +0200

    update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,12 @@
 -- full description
 USE hbtn_0c_0;
-CREATE TABLE `first_table` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(128) DEFAULT NULL,
-    `c` CHAR(1) DEFAULT NULL,
-    `created_at` DATE DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+first_tableCREATETABLE`first_table`(
+`id`intNOTNULLAUTO_INCREMENT,
+`name`varchar(128)DEFAULTNULL,
+`c`char(1)DEFAULTNULL,
+`created_at`dateDEFAULTNULL,
+PRIMARYKEY(`id`)
+)ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci
 
 
 
