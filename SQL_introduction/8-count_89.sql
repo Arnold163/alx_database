7edb1af0b7287769a40ccaebcfec70101f6c5ede
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-04 21:22:13 +0200

    update

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -5,10 +5,7 @@ CREATE TABLE IF NOT EXISTS first_table (
     id INT PRIMARY KEY,
     name VARCHAR(256)
 );
-INSERT INTO first_table (id, name) VALUES 
-(89, 'Holberton School'),
-(89, 'Holberton School'),
-(89, 'Holberton School');
+
 SELECT COUNT(*) FROM first_table WHERE id = 89;
 
 
