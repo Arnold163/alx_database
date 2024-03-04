59a425eb9610b9547b5fae58cbe80519ad7e64ce
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-04 20:41:58 +0200

    update

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,7 @@
 -- list all values
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_6;
 USE hbtn_test_db_6;
+DROP TABLE IF EXISTS first_table;
 CREATE  TABLE IF NOT EXISTS first_table(
     id INT PRIMARY KEY,
     name VARCHAR(256)
