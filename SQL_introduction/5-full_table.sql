8284b627d847cf1f6558c54c6a11d5a26d0e8b9f
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-04 12:31:51 +0200

    update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,8 +9,9 @@ SELECT
   EXTRA AS Extra
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
 
-SHOW CREATE 'first_table';
+SHOW CREATE first_table;
 
 
 
