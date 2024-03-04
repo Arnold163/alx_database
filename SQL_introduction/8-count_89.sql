d49cd439e37baa7dccc4b834ff89ca2be8abd9df
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-04 21:14:11 +0200

    update

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,6 +1,10 @@
 -- mysql count
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(256)
+);
 SELECT COUNT(*) FROM first_table WHERE id = 89;
 
 
